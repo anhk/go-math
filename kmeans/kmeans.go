0639634f8825b5520badcff62376503bb67369b3
anhk <anhongkui@jd.com>
2021-09-26 17:49:25 +0800

    update kmeans

---
+++ kmeans/kmeans.go
@@ -0,0 +1,140 @@
+package kmeans
+
+import (
+	"math"
+	"math/rand"
+)
+
+/**
+ * 聚类算法-kmeans with Distance(Euclidean)
+ */
+
+type Point struct {
+	X, Y float64
+}
+
+const float64EqualityThreshold = 1e-9
+
+func almostEqual(a, b float64) bool {
+	return math.Abs(a-b) <= float64EqualityThreshold
+}
+
+func (p Point) DistanceTo(t Point) float64 {
+	return math.Sqrt(math.Pow(t.X-p.X, 2) + math.Pow(t.Y-p.Y, 2))
+}
+
+func (p Point) Equals(x float64, y float64) bool {
+	return almostEqual(p.X, x) && almostEqual(p.Y, y)
+}
+
+type KMeans struct {
+	k        int
+	clusters []Cluster
+}
+
+type Cluster struct {
+	centroide     Point // 中心点
+	isConvergence bool
+	Points        []Point // 聚类内的点
+}
+
+func (c *Cluster) addPoint(v Point) {
+	c.Points = append(c.Points, v)
+}
+
+func NewKMeans(k int) *KMeans {
+	return &KMeans{k: k}
+}
+
+// initCentroides: 初始化k个质心点
+func (k *KMeans) initCentroides(points []Point) {
+	k.clusters = make([]Cluster, k.k)
+
+	// 找到数据集的范围
+	maxX := math.SmallestNonzeroFloat64
+	maxY := math.SmallestNonzeroFloat64
+	minX := math.MaxFloat64
+	minY := math.MaxFloat64
+
+	for _, v := range points {
+		maxX = math.Max(maxX, v.X)
+		maxY = math.Max(maxY, v.Y)
+		minX = math.Min(minX, v.X)
+		minY = math.Min(minY, v.Y)
+	}
+
+	// 在范围内随机初始化k个中心点
+	for i := 0; i < k.k; i++ {
+		x := rand.Float64()*(maxX-minX) + minX
+		y := rand.Float64()*(maxY-minY) + minY
+		k.clusters[i].centroide = Point{X: x, Y: y}
+	}
+}
+
+// isConvergence: 是否收敛
+func (k *KMeans) isConvergence() bool {
+	for _, v := range k.clusters {
+		if !v.isConvergence {
+			return false
+		}
+	}
+	return true
+}
+
+func (k *KMeans) Run(points []Point) []Cluster {
+	k.initCentroides(points)
+	for !k.isConvergence() {
+		k.initCluster()          // 重置分类中的点
+		k.classifyPoint(points)  // 计算距离进行分类
+		k.recalcularCentroides() // 重新计算质心点
+	}
+
+	return k.clusters
+}
+
+// 重置分类中的点
+func (k *KMeans) initCluster() {
+	for i := 0; i < k.k; i++ {
+		k.clusters[i].Points = nil
+	}
+}
+
+// classifyPoint: 计算距离进行分类
+func (k *KMeans) classifyPoint(points []Point) {
+	for _, v := range points {
+		masCercano := 0
+		minDistancia := math.MaxFloat64
+		for i := 0; i < k.k; i++ {
+			distancia := v.DistanceTo(k.clusters[i].centroide)
+			if minDistancia > distancia {
+				minDistancia = distancia
+				masCercano = i
+			}
+		}
+		k.clusters[masCercano].addPoint(v)
+	}
+}
+
+// recalcularCentroides: 重新计算质心点
+func (k *KMeans) recalcularCentroides() {
+	for i := 0; i < k.k; i++ {
+		if len(k.clusters[i].Points) == 0 {
+			k.clusters[i].isConvergence = true
+			continue
+		}
+
+		// 求均值，作为新的质心点
+		var x, y, sumX, sumY float64
+		for _, v := range k.clusters[i].Points {
+			sumX += v.X
+			sumY += v.Y
+		}
+		x = sumX / float64(len(k.clusters[i].Points))
+		y = sumY / float64(len(k.clusters[i].Points))
+		if k.clusters[i].centroide.Equals(x, y) {
+			k.clusters[i].isConvergence = true
+		} else {
+			k.clusters[i].centroide = Point{X: x, Y: y}
+		}
+	}
+}
---
+++ kmeans/kmeans_test.go
@@ -0,0 +1,25 @@
+package kmeans
+
+import (
+	"fmt"
+	"testing"
+)
+
+var (
+	P = []Point{{1.5, 2}, {1, 3}, {2, 4}, {9, 19}}
+	Q = []Point{{1, 2}, {1, 3}, {1, 4}, {1, 19}}
+)
+
+func TestKMeans(t *testing.T) {
+	k := NewKMeans(4)
+	c := k.Run(P)
+	for _, cc := range c {
+		fmt.Println(cc.centroide, cc.Points)
+	}
+
+	fmt.Println("++++")
+	c2 := k.Run(Q)
+	for _, cc := range c2 {
+		fmt.Println(cc.centroide, cc.Points)
+	}
+}
